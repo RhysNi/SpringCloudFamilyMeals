e87a36adb653ffa16195e06be4bf31cc63665224
RhysNi <java_nsd@163.com>
2022-09-23 04:04:30 +0800

    Feign & Ribbon超时重试机制

--- Feign/Feign-Consumer/src/main/java/com/rhys/feignconsumer/controller/FeignConsumerController.java
+++ Feign/Feign-Consumer/src/main/java/com/rhys/feignconsumer/controller/FeignConsumerController.java
@@ -2,7 +2,6 @@ package com.rhys.feignconsumer.controller;
 
 import com.rhys.feignconsumer.service.FeignConsumerApi;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
--- Feign/Feign-Provider/src/main/java/com/rhys/feignprovider/controller/FeignProviderController.java
+++ Feign/Feign-Provider/src/main/java/com/rhys/feignprovider/controller/FeignProviderController.java
@@ -1,7 +1,11 @@
 package com.rhys.feignprovider.controller;
 
 import com.rhys.serviceapi.Service.ServiceApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Rhys.Ni
@@ -10,9 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FeignProviderController implements ServiceApi {
+    private AtomicInteger requestCount = new AtomicInteger();
+
+    @Value("${server.port}")
+    private String port;
 
     @Override
     public String pingFeignProvider() {
-        return "Ping Feign Provider Success";
+        // try {
+        //     System.out.println("开始模拟超时");
+        //     TimeUnit.MILLISECONDS.sleep(5000);
+        // } catch (InterruptedException e) {
+        //     throw new RuntimeException();
+        // }
+        return "Ping Feign Provider Port:" + port + " Success Count:"+requestCount.incrementAndGet();
     }
 }
